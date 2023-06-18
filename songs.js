ac1d57034f84130755d3517487d57080a50aef40
Ekansh <zethyst@protonmail.com>
2023-06-19 01:36:57 +0530

    Added new songs

--- songs.js
+++ songs.js
@@ -1,62 +1,74 @@
 let songlist=[
     {
-        name:"Sunflower",
-        artist:"Post Malone & Swae Lee",
+        name:"Home",
+        artist:"Vince Staples",
         src:"song1",
         img:"cover1"
     },
     {
-        name:"Delta",
-        artist:"Yamaha",
+        name:"Am I Dreaming",
+        artist:"Metro Boomin & A$AP Rocky",
         src:"song2",
         img:"cover2"
     },
     {
-        name:"There you are",
-        artist:"Zayn Malik",
+        name:"Yeh Raaten Yeh Mausam",
+        artist:"Sanam, Simran Sehgal",
         src:"song3",
         img:"cover3"
     },
     {
-        name:"Midnight",
-        artist:"Coldplay",
+        name:"Kabira",
+        artist:"Pritam",
         src:"song4",
         img:"cover4"
     },
     {
-        name:"Birds",
-        artist:"Imagine Dragons",
+        name:"There you are",
+        artist:"Zayn Malik",
         src:"song5",
         img:"cover5"
     },
     {
-        name:"Castle of Glass",
-        artist:"Linkin Park",
+        name:"Midnight",
+        artist:"Coldplay",
         src:"song6",
         img:"cover6"
     },
     {
-        name:"See You Again",
-        artist:"Wiz Khalifa & Charlie Puth",
+        name:"Birds",
+        artist:"Imagine Dragons",
         src:"song7",
         img:"cover7"
     },
     {
-        name:"Tangled Up",
-        artist:"Parade of Lights",
+        name:"Castle of Glass",
+        artist:"Linkin Park",
         src:"song8",
         img:"cover8"
     },
     {
-        name:"Midnight City",
-        artist:"M83",
+        name:"See You Again",
+        artist:"Wiz Khalifa & Charlie Puth",
         src:"song9",
         img:"cover9"
     },
     {
-        name:"All We Know",
-        artist:"The Chainsmokers",
+        name:"Tangled Up",
+        artist:"Parade of Lights",
         src:"song10",
         img:"cover10"
+    },
+    {
+        name:"Midnight City",
+        artist:"M83",
+        src:"song11",
+        img:"cover11"
+    },
+    {
+        name:"All We Know",
+        artist:"The Chainsmokers",
+        src:"song12",
+        img:"cover12"
     },
 ]
